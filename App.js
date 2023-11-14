540fdc2614c084b72c60db1c490250c088637188
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-14 13:46:25 -0600

    Creando el componente del FORMULARIO

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {
   StyleSheet,
   Image,
+  View,
 } from 'react-native';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 const App = () => {
   return (
@@ -14,6 +16,12 @@ const App = () => {
         style={styles.imagen}
         source={ require('./assets/img/cryptomonedas.png') }
       />
+
+      <View style={styles.contenido}>
+        <Formulario
+
+        />
+      </View>
     </>
   );
 };
@@ -24,6 +32,10 @@ const styles = StyleSheet.create
     imagen: {
       width: '100%',
       height: 150,
+      marginHorizontal: '2.5%',
+    },
+
+    contenido: {
       marginHorizontal: '2.5%',
     },
   }

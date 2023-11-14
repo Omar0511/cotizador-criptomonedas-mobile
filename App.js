1d514c34369d500da8bdb74836a4237e0a17216d
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-14 13:38:04 -0600

    Mostrando la IMG

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Image,
 } from 'react-native';
 import Header from './components/Header';
 
@@ -14,6 +9,11 @@ const App = () => {
   return (
     <>
       <Header />
+
+      <Image
+        style={styles.imagen}
+        source={ require('./assets/img/cryptomonedas.png') }
+      />
     </>
   );
 };
@@ -21,7 +21,11 @@ const App = () => {
 const styles = StyleSheet.create
 (
   {
-    
+    imagen: {
+      width: '100%',
+      height: 150,
+      marginHorizontal: '2.5%',
+    },
   }
 );
 
